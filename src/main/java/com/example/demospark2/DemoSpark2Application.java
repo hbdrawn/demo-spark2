c122d36d94fd176091587c9c2c0ef6f2e54b487f
hbdrawn <hbdrawn@vip.qq.com>
2018-09-27 19:53:14 +0800

    项目初始化

---
+++ src/main/java/com/example/demospark2/DemoSpark2Application.java
@@ -0,0 +1,17 @@
+package com.example.demospark2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ *  本demo主要是为spark2脚本的编写和测试
+ * @author hbdrawn
+ * @date
+ */
+@SpringBootApplication
+public class DemoSpark2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpark2Application.class, args);
+    }
+}
---
+++ src/test/java/com/example/demospark2/DemoSpark2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demospark2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpark2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
